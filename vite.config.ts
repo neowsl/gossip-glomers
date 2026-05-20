17d9b524dc1a09c6aac6d5bd6fa42a9056f22227
Neal Wang <nealwang.sh@protonmail.com>
2026-05-20 00:57:04 -0700

    feat: github pages setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,26 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	resolve: {
-		alias: {
-			"@": path.resolve(import.meta.dirname, "src"),
-			"@assets": path.resolve(
-				import.meta.dirname,
-				"..",
-				"..",
-				"attached_assets",
-			),
+export default defineConfig(({ command }) => {
+	return {
+		base: command === "build" ? "/gossip-glomers/" : "/",
+		plugins: [react(), tailwindcss()],
+		resolve: {
+			alias: {
+				"@": path.resolve(import.meta.dirname, "src"),
+				"@assets": path.resolve(
+					import.meta.dirname,
+					"..",
+					"..",
+					"attached_assets",
+				),
+			},
+			dedupe: ["react", "react-dom"],
 		},
-		dedupe: ["react", "react-dom"],
-	},
-	root: path.resolve(import.meta.dirname),
-	build: {
-		outDir: path.resolve(import.meta.dirname, "dist/public"),
-		emptyOutDir: true,
-	},
+		root: path.resolve(import.meta.dirname),
+		build: {
+			outDir: path.resolve(import.meta.dirname, "dist/public"),
+			emptyOutDir: true,
+		},
+	};
 });
